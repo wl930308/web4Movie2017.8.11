5ddbf50d65fb03cfa6af5164f9e78cc401e8e922
王磊 <836017252@qq.com>
2017-08-15 08:47:49 +0800

    2017.8.15提交

---
+++ web4Movie/src/js/houTaiZhuYe.js
@@ -0,0 +1,12 @@
+require('../css/houTaiZhuYe.css');
+//监听socket传来的信息
+var socket = io.connect();
+socket.on('reload',function(){
+	//刷新网页
+	window.location.reload();
+});
+require("jquery");
+require("bootstrap");
+require("bootstrap-loader");
+
+$("#xiaLa").dropdown();
---
+++ web4Movie/src/js/movieList.js
@@ -0,0 +1,10 @@
+require('../css/movieList.css');
+//监听socket传来的信息
+var socket = io.connect();
+socket.on('reload',function(){
+	//刷新网页
+	window.location.reload();
+});
+require("jquery");
+require("bootstrap");
+require("bootstrap-loader");
